62d3b3f434a1bf1edebbcc792c5fdc2ce8a33352
khusboo-kumari <dyttokhus@gmail.com>
2024-10-01 23:05:07 +0530

    updated Profile Responsive

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ['./**/*.html'],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [], 
+// }
+
+module.exports = {
+  content: ["./*.html"], // Add paths to your HTML and JS files here
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
